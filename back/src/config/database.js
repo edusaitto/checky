50dc6fd128bbb3c99e37de12b1aaa916575b2ce4
Eduardo Saito <61482919+edusaitto@users.noreply.github.com>
2021-01-24 01:15:00 -0300

    Update database.js

--- back/src/config/database.js
+++ back/src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
     try {
         mongoose.connect(
-            'mongodb://saito:saito123@kamino.mongodb.umbler.com:48199/tasks', 
+            'mongodb://saito:saitochecky@cluster0.zwpoj.mongodb.net/checky', 
             { useNewUrlParser: true }
             )
     } catch (error) {
