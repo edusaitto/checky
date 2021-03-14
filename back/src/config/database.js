0274f6cc037842d48461b132c3d77ddfe1e80cb3
Eduardo Saito <61482919+edusaitto@users.noreply.github.com>
2021-03-14 11:14:48 -0300

    Update database.js

--- back/src/config/database.js
+++ back/src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
     try {
         mongoose.connect(
-            'mongodb://saito:Saito1502@geonosis.mongodb.umbler.com:48124', 
+            'mongodb://saito:Saito1502@mongo_checkydb:27017', 
             { useNewUrlParser: true }
             )
     } catch (error) {
